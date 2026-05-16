5fd769c7324b46f0f8ade4c1fe8570c62fcf4150
sidduttala0505 <148736474+sidduttala0505@users.noreply.github.com>
2026-05-16 10:42:00 -0400

    initial commit: gradle + spring boot skeleton

---
+++ src/main/java/com/sidduttala/hypeclipper/HypeClipperApplication.java
@@ -0,0 +1,12 @@
+package com.sidduttala.hypeclipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeClipperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypeClipperApplication.class, args);
+    }
+}
